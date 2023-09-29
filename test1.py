80671998ef0b42ba37c9e79e01de505ef97d271a
demoworkgenius <demoworkgenius@gmail.com>
2023-09-30 00:36:43 +0600

    Updated response

--- test1.py
+++ test1.py
@@ -1,10 +1,14 @@
 import speech_recognition as sr
 import pyttsx3
-import sys  # Import the sys module
+import nltk
+from nltk.tokenize import word_tokenize
 
-# Define the wake words
-WAKE_WORDS = ["nova", "hey nova", "are you there nova", "hello"]
-EXIT_COMMANDS = ["close", "shutdown", "exit"]
+# Define wake words
+WAKE_WORDS = ["kotha", "hi kotha", "hey kotha", "hello"]
+EXIT_COMMANDS = ["close", "shutdown", "exit", "terminate"]
+
+# Initialize NLTK
+nltk.download("punkt")
 
 def get_female_voice(engine):
     voices = engine.getProperty('voices')
@@ -20,65 +24,78 @@ def speak_text(text):
 
 def main():
     recognizer = sr.Recognizer()
+    conversation = []
 
-    program_active = True
-    while program_active:
+    program_active = False
+    response = "Hi master! How can I assist you today?"
+    speak_text(response)
+    print("Kotha: " + response)
+
+    while True:
         try:
             with sr.Microphone() as source:
-                print("Listening... Say something:")
+                print("Listening...")  # Print "Listening..." message when actively listening
                 recognizer.adjust_for_ambient_noise(source)
-                audio = recognizer.listen(source, timeout=5, phrase_time_limit=2)
+                audio = recognizer.listen(source, timeout=10, phrase_time_limit=5)
 
             text = recognizer.recognize_google(audio).lower()
+            print(f"You: {text}")  # Print what you say to Kotha
 
-            # Check if any wake word is in the recognized text
-            if any(wake_word in text for wake_word in WAKE_WORDS):
-                response = "Yes master, How can I help you?"
-                print(f"NOVA: {response}")
+            # Check if the wake word is recognized
+            if any(wake_word in text for wake_word in WAKE_WORDS) and not program_active:
+                program_active = True
+                response = "Yes, I'm here. How can I assist you today?"
                 speak_text(response)
+                print("Kotha: " + response)
+                continue  # Skip the rest of the loop for wake word
 
-                # Listen for a command (create a new source)
-                with sr.Microphone() as command_source:
-                    print("Listening for a command...")
-                    recognizer.adjust_for_ambient_noise(command_source)
-                    command_audio = recognizer.listen(command_source, timeout=5, phrase_time_limit=5)
+            # If program_active is False, continue listening for wake word
+            if not program_active:
+                continue
 
-                command = recognizer.recognize_google(command_audio).lower()
+            conversation.append(text)
+            response = process_command(conversation)
 
-                # Check for exit commands
-                if any(exit_command in command for exit_command in EXIT_COMMANDS):
-                    response = "Have a good day!"
-                    print(f"NOVA: {response}")
-                    speak_text(response)
-                    program_active = False  # Exit the program
-                else:
-                    if "good morning" in command or "morning" in command:
-                        response = "Good morning!"
-                    elif "hear me" in command or "listen to me" in command:
-                        response = "Yes master, I am listening to you."
-                    elif "who are you" in command or "about yourself" in command:
-                        response = "I'm just a computer program, but thanks for asking!"
-                    elif "thank you" in command:
-                        response = "You're welcome!"
-                    else:
-                        response = "I'm not sure how to respond to that."
+            if response:
+                print(f"Kotha: {response}")
+                speak_text(response)
+                conversation = []  # Clear conversation after Kotha's response
 
-                    print(f"You said: {command}")
-                    print(f"NOVA: {response}")
-                    speak_text(response)
+            # Check for exit commands
+            elif any(exit_command in text for exit_command in EXIT_COMMANDS):
+                response = "Have a good day, master!"
+                print(response)
+                speak_text(response)
+                break  # Exit the program
+            
+            else:
+                # Generate a response for not understanding
+                not_understood_response = "I'm sorry, I didn't quite catch that. Can you please repeat?"
+                print(f"Kotha: {not_understood_response}")
+                speak_text(not_understood_response)
 
         except sr.WaitTimeoutError:
-            response = "Timeout: No speech detected. Please say again..."
-            print(response)
-            speak_text(response)
+            pass  # Continue listening
         except sr.UnknownValueError:
-            response = "Sorry, I couldn't understand what you said. Please say again..."
+            response = "I'm sorry, I didn't quite catch that. Can you please repeat?"
             print(response)
             speak_text(response)
-        except sr.RequestError as e:
-            response = f"Sorry, there was an error with the request: {str(e)}. Please say again..."
-            print(response)
-            speak_text(response)
+
+def process_command(conversation):
+    # Convert the conversation into a single string
+    conversation_text = ' '.join(conversation)
+
+    # Tokenize the text
+    tokens = word_tokenize(conversation_text)
+
+    if "good morning" in conversation_text or "morning" in conversation_text:
+        return "Good morning!"
+    elif "who are you" in conversation_text or "about yourself" in conversation_text:
+        return "Thanks for your curiosity. My name is Kotha, and I'm here to assist you."
+    elif "thank you" in conversation_text:
+        return "You're welcome!"
+    else:
+        return None  # Return None for no response
 
 if __name__ == "__main__":
     main()
